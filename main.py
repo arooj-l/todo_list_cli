4084b3d7a77c89224cd45241b5f28ee0e863f8c6
arooj-l <aliaqat@udel.edu>
2026-03-04 06:45:09 -0500

    Improve Todo List with validation and advanced list handling

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
 
 # initialize list to store tasks
 tasks = []
+try:
+    while True:
 
-while True:
-    print("""
+     print("""
         \n***** Todo list Menu *****
                 1: ADD task
                 2: VIEW task
                 3: REMOVE task
                 4: EXIT
       """)
+     
     
-    choice = input("choose an option(1-4): ")
+     choice = input("choose an option(1-4): ")
 
-    if choice == "1":
+     if choice == "1":
         task  = input("Enter a task: ")
         tasks.append(task)
         print("task added")
 
-    elif choice == "2":
+     elif choice == "2":
         print("\nYour tasks:")
         for index, task in enumerate(tasks, start =1):
             print(f"{index}.{task}")
     
-    elif choice == "3":
+     elif choice == "3":
+        if not tasks:
+           print("The task is list is empty")
         task_num = int(input("Enter task number to remove: "))
         removed = tasks.pop(task_num - 1)
         print(f"Removed: {removed}")
 
-    elif choice == "4":
-        print("Goodbye!")
-        break
+     elif choice == "4":
+         print("Goodbye!")
+         break
+     elif choice == "5":
+         tasks.clear()
+         print("List is clear now")
+    
 
-    else:
+
+     else:
         print("Invalid choice. Try again.")
+except ValueError:
+    print("Enter a valid number")
