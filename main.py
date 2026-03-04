37bcd8bdf4898d260435e59eea679b62ee49c5e3
arooj-l <aliaqat@udel.edu>
2026-03-04 06:55:22 -0500

    improvements

--- main.py
+++ main.py
@@ -1,47 +1,52 @@
 
 # initialize list to store tasks
 tasks = []
-try:
-    while True:
 
-     print("""
+while True:
+    print("""
         \n***** Todo list Menu *****
                 1: ADD task
                 2: VIEW task
                 3: REMOVE task
                 4: EXIT
+                5:CLEAR all tasks
       """)
      
     
-     choice = input("choose an option(1-4): ")
+    choice = input("choose an option(1-4): ")
 
-     if choice == "1":
+    if choice == "1":
         task  = input("Enter a task: ")
         tasks.append(task)
         print("task added")
 
-     elif choice == "2":
+    elif choice == "2":
         print("\nYour tasks:")
-        for index, task in enumerate(tasks, start =1):
-            print(f"{index}.{task}")
+        if not tasks:
+            print("No tasks yet.")
+        else:
+            for index, task in enumerate(tasks, start =1):
+                print(f"{index}.{task}")
     
-     elif choice == "3":
+    elif choice == "3":
         if not tasks:
            print("The task is list is empty")
-        task_num = int(input("Enter task number to remove: "))
-        removed = tasks.pop(task_num - 1)
-        print(f"Removed: {removed}")
+        else:
+            try:
+                task_num = int(input("Enter task number to remove: "))
+                if 1 <= task_num <= len(tasks):
+                    removed = tasks.pop(task_num - 1)
+                    print(f"Removed: {removed}")
+                else:
+                    print("Invalid task number.")
+            except ValueError:
+                print("Please enter a valid number.")
+                
 
-     elif choice == "4":
+    elif choice == "4":
          print("Goodbye!")
          break
-     elif choice == "5":
+    elif choice == "5":
          tasks.clear()
          print("List is clear now")
     
-
-
-     else:
-        print("Invalid choice. Try again.")
-except ValueError:
-    print("Enter a valid number")
